d854a2149159e6b9c1b2471d85e2a2ad571768de
Andres Delgado <adelgado0723@gmail.com>
2019-04-26 02:41:16 +0000

    Changed bio

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-cp -r index.html ../html/index.php
-cp -r assets/ ../html/
-cp -r index_files/ ../html/
+sudo cp -r index.html ../html/index.php
+sudo cp -r assets/ ../html/
+sudo cp -r Andy-Delgado.com_files/ ../html/
