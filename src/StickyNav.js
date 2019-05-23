ae5cde22bd575c5166364411948ed6e45fa2db03
Andres Delgado <adelgado0723@gmail.com>
2019-05-23 00:24:46 -0400

    Worked on styling sticky nav

--- src/StickyNav.js
+++ src/StickyNav.js
@@ -4,10 +4,20 @@ let topOfNav = navLinks.offsetTop;
 function fixNav() {
   if (window.scrollY >= topOfNav) {
     document.body.style.paddingTop = navLinks.offsetHeight + 'px';
+    // document.body.classList.add('follow-off');
+
     document.body.classList.add('fixed-nav');
+    // document.body.one('transitioned', function() {
+    //   document.body.classList.remove('follow-off');
+    // });
   } else {
+    // document.body.classList.add('follow-off');
     document.body.classList.remove('fixed-nav');
+    // document.body.one('transitioned', function() {
+    //   document.body.classList.remove('follow-off');
+    // });
     document.body.style.paddingTop = 0;
+    // document.body.style.top = 0;
   }
 }
 
