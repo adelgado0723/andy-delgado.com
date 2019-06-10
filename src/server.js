dec7b4a2cdc1043d2fcea5a1f7306439618128b1
Andres Delgado <adelgado0723@gmail.com>
2019-06-09 23:13:02 -0400

    Added validation and sanitization of user input in form using express-validator

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const { check, validationResult } = require('express-validator/check');
 
 const app = express();
 
@@ -13,32 +14,50 @@ app.get('/', function(req, res) {
   res.type('html');
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.post('/contact', (req, res) => {
-  console.log(req);
-  let mailOpts, smtpTrans;
-  smtpTrans = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.GMAIL_USR,
-      pass: process.env.GMAIL_PASS,
-    },
-  });
-  mailOpts = {
-    from: req.body.name + ' &lt;' + req.body.email + '&gt;',
-    to: process.env.GMAIL_USR,
-    subject: 'New message from contact form at andy-delgado.com',
-    text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`,
-  };
-  smtpTrans.sendMail(mailOpts, function(error, response) {
-    if (error) {
-      res.end('contact-failure');
-    } else {
-      res.end('contact-success');
+app.post(
+  '/contact',
+  [
+    check('name')
+      .trim()
+      .escape(),
+    check('email')
+      .isEmail()
+      .normalizeEmail(),
+    check('message')
+      .trim()
+      .escape(),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
     }
-  });
-});
+    console.log(req);
+    let mailOpts, smtpTrans;
+    smtpTrans = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.GMAIL_USR,
+        pass: process.env.GMAIL_PASS,
+      },
+    });
+    mailOpts = {
+      from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+      to: process.env.GMAIL_USR,
+      subject: 'New message from contact form at andy-delgado.com',
+      text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`,
+    };
+    smtpTrans.sendMail(mailOpts, function(error, response) {
+      if (error) {
+        res.end('contact-failure');
+      } else {
+        res.end('contact-success');
+      }
+    });
+  }
+);
 
 app.listen(3000);
 console.log('listening on http://localhost:3000');
